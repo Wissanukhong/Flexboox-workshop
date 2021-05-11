e99c90ae1f214e3ebca75a3e7187a8e027113e3b
Wissanu.khong <wissanu.khong@gmail.com>
2021-05-11 07:18:39 +0700

    react-router-dom

--- react-website/src/components/Navbar.js
+++ react-website/src/components/Navbar.js
@@ -1,10 +1,15 @@
 import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 function Navbar() {
   return (
     <>
       <nav className='navbar'>
-        <div className='navbar-container'></div>
+        <div className='navbar-container'>
+          <Link to='/' className='navbar-logo'>
+            Travel <i class='fas fa-plane'></i>
+          </Link>
+        </div>
       </nav>
     </>
   );
