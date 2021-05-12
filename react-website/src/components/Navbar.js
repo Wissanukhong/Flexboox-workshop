2d9832c785a8096cb2502ec2cf43dd8e4bc29ed4
Wissanu.khong <wissanu.khong@gmail.com>
2021-05-12 07:59:27 +0700

    create function toggle menu icons

--- react-website/src/components/Navbar.js
+++ react-website/src/components/Navbar.js
@@ -1,12 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 function Navbar() {
+  // set useSate
+  const [click, setClick] = useState(false);
+
+  // function handleClick
+  const handleClick = () => setClick(!click);
+
   return (
     <>
       <nav className='navbar'>
         <div className='navbar-container'>
-          <Link></Link>
+          {/* logo */}
+          <Link to='/' className='navbar-logo'>
+            Travel <i class='fas fa-route'></i>
+          </Link>
+
+          {/* menu icon function toggle */}
+          <div className='menu-icon' onClick={handleClick}>
+            <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
+          </div>
         </div>
       </nav>
     </>
